f7c2c3c7ff35b10312ab778f0d7363bbf2199142
SubrataInnovates <subra721131@gmail.com>
2026-06-27 17:25:30 +0530

    PBCS -015 : Implement createCard Bussiness Logic

--- src/main/java/com/pbcs/card/service/impl/CardServiceImpl.java
+++ src/main/java/com/pbcs/card/service/impl/CardServiceImpl.java
@@ -6,12 +6,15 @@ import org.springframework.stereotype.Service;
 
 import com.pbcs.card.dto.request.CreateCardRequest;
 import com.pbcs.card.dto.response.CardResponse;
+import com.pbcs.card.entity.Card;
+import com.pbcs.card.enums.CardStatus;
 import com.pbcs.card.mapper.CardMapper;
 import com.pbcs.card.repository.CardRepository;
 import com.pbcs.card.service.CardService;
 import com.pbcs.card.util.CardNumberGenerator;
 import com.pbcs.card.util.CvvGenerator;
 
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -22,10 +25,17 @@ public class CardServiceImpl implements CardService
 	private final CardMapper cardMapper;
 
 	@Override
+	@Transactional
 	public CardResponse createCard(CreateCardRequest request)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		Card card = cardMapper.toEntity(request);
+		card.setCardNumber(generateUniqueCardNumber());
+		card.setCvv(generateCvv());
+		card.setBalance(request.getInitialBalance());
+		card.setStatus(CardStatus.ACTIVE);
+		
+		Card savedCard = cardRepository.save(card);
+		return cardMapper.toResponse(savedCard);
 	}
 
 	@Override
